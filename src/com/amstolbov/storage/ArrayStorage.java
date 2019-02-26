4e75cf232e5b71c249ddd4943f1fbd681697da62
AlexStolbov <stolbov_alexey@mail.ru>
2019-02-26 12:12:33 +0300

    HW2. remove comment from Resume

--- src/com/amstolbov/storage/ArrayStorage.java
+++ src/com/amstolbov/storage/ArrayStorage.java
@@ -1,32 +1,18 @@
 package com.amstolbov.storage;
 
 import com.amstolbov.model.Resume;
+
 import java.util.Arrays;
 
-/**
- * Array based storage for Resumes
- */
 public class ArrayStorage {
     private Resume[] storage = new Resume[10000];
-    /**
-     * Index of last resume.
-     */
     private int lastIndex = -1;
 
-    /**
-     * Clear storage.
-     */
     public void clear() {
         Arrays.fill(storage, 0, lastIndex, null);
         lastIndex = -1;
     }
 
-    /**
-     * Adds the specified item to the repository.
-     * Check that repository is full.
-     * Verifies that uuid is corrert.
-     * @param r resume for add.
-     */
     public void save(Resume r) {
         if (lastIndex == storage.length - 1) {
             System.out.println("ERROR save: storage is full");
@@ -42,11 +28,6 @@ public class ArrayStorage {
         }
     }
 
-    /**
-     * Returns an item by uuid.
-     * @param uuid search identifier.
-     * @return resume or null
-     */
     public Resume get(String uuid) {
         int findIndex = getIndex(uuid);
         if (findIndex > -1) {
@@ -57,10 +38,6 @@ public class ArrayStorage {
         return null;
     }
 
-    /**
-     * Removes the element with the given uuid, if it exists. And compresses the storage.
-     * @param uuid - uuid of the item to remove.
-     */
     public void delete(String uuid) {
         int findIndex = getIndex(uuid);
         if (findIndex > -1) {
@@ -72,17 +49,10 @@ public class ArrayStorage {
         }
     }
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
     public Resume[] getAll() {
         return Arrays.copyOf(storage, lastIndex + 1);
     }
 
-    /**
-     * The lastIndex of the storage without empty items.
-     * @return lastIndex
-     */
     public int size() {
         return lastIndex + 1;
     }
@@ -96,11 +66,6 @@ public class ArrayStorage {
         }
     }
 
-    /**
-     * Returns the index of the item with the given uuid or -1 if not found.
-     * @param uuid - find value
-     * @return index of uuid
-     */
     private int getIndex(String uuid) {
         for (int i = 0; i <= lastIndex; i++) {
             if (storage[i].getUuid().equals(uuid)) {
