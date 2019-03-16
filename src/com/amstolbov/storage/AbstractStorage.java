14a79181a9d3e9f113955ad6415b51cce900a290
AlexStolbov <stolbov_alexey@mail.ru>
2019-03-16 08:50:21 +0300

    HW5.2. Refactoring methods save and checkForExistElement

--- src/com/amstolbov/storage/AbstractStorage.java
+++ src/com/amstolbov/storage/AbstractStorage.java
@@ -5,32 +5,30 @@ import com.amstolbov.exception.NotExistStorageException;
 import com.amstolbov.model.Resume;
 
 public abstract class AbstractStorage implements Storage {
+    private final String FIND_EXIST = "FIND_EXIST";
+    private final String FIND_EMPTY = "FIND_EMPTY";
 
     @Override
     public void update(Resume resume) {
-        Object existPosition = checkForExistElement(resume.getUuid());
+        Object existPosition = checkForExistElement(resume.getUuid(), FIND_EXIST);
         updateElement(existPosition, resume);
     }
 
     @Override
     public void save(Resume resume) {
-        Object existPosition = getExistPosition(resume.getUuid());
-        if (elementExistInThisPosition(existPosition)) {
-            throw new ExistStorageException(resume.getUuid());
-        } else {
-            saveElement(resume, existPosition);
-        }
+        Object existPosition = checkForExistElement(resume.getUuid(), FIND_EMPTY);
+        saveElement(resume, existPosition);
     }
 
     @Override
     public Resume get(String uuid) {
-        Object existPosition = checkForExistElement(uuid);
+        Object existPosition = checkForExistElement(uuid, FIND_EXIST);
         return getElement(existPosition);
     }
 
     @Override
     public void delete(String uuid) {
-        Object existPosition = checkForExistElement(uuid);
+        Object existPosition = checkForExistElement(uuid, FIND_EXIST);
         deleteElement(existPosition);
     }
 
@@ -46,12 +44,15 @@ public abstract class AbstractStorage implements Storage {
 
     protected abstract boolean elementExistInThisPosition(Object existPosition);
 
-    private Object checkForExistElement(String uuid) {
+    private Object checkForExistElement(String uuid, String findType) {
         Object existPosition = getExistPosition(uuid);
-        if (!elementExistInThisPosition(existPosition)) {
+        boolean elementExist = elementExistInThisPosition(existPosition);
+        if (findType.equals(FIND_EXIST) && !elementExist) {
             throw new NotExistStorageException(uuid);
-        } else {
-            return existPosition;
         }
+        if (findType.equals(FIND_EMPTY) && elementExist) {
+            throw new ExistStorageException(uuid);
+        }
+        return existPosition;
     }
 }
