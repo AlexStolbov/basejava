cff69106e29694b7a0fc17d35e4d7b69196a1ff5
AlexStolbov <stolbov_alexey@mail.ru>
2019-03-31 13:52:27 +0300

    HW7. Add javadoc in ResumeSectionAbstract

--- src/com/amstolbov/model/ResumeSectionAbstract.java
+++ src/com/amstolbov/model/ResumeSectionAbstract.java
@@ -1,9 +1,20 @@
 package com.amstolbov.model;
 
+/**
+ * Resume section.
+ * @param <T> - type of information in section
+ */
 public abstract class ResumeSectionAbstract<T> {
 
+    /**
+     * Add new information in section.
+     */
     public abstract void addSectionPart(T sectionPart);
 
+    /**
+     * Create representation of section.
+     * @return - view of section
+     */
     public abstract String createRepresentation();
 
     @Override
